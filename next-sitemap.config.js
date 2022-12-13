e4f88e2d8a6e1af94c61ad25ad4304b6700b3347
Jeremy <jeremydouchamps@gmail.com>
2022-12-13 15:16:10 +0700

    Update site url

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,5 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: process.env.SITE_URL || 'https://devnet-ghostverse.netlify.app/',
+  siteUrl: process.env.SITE_URL || 'https://green.gd',
   generateRobotsTxt: true,
 };
