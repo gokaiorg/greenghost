406ffdbc2974c7a80ac716612520042c749ee2ce
Jeremy <jeremydouchamps@gmail.com>
2022-12-18 12:37:09 +0700

    Update social links

--- components/SocialMediaIcons.tsx
+++ components/SocialMediaIcons.tsx
@@ -6,14 +6,14 @@ export const SocialMediaIcons = () => {
     <Box display="flex" alignItems="center" gap={3}>
       <Box bgColor="black" borderRadius="100%">
         <SocialIcon
-          url="https://www.facebook.com/GokaiLabs"
+          url="https://www.facebook.com/greengardencs"
           bgColor="#4cfd27"
           style={{ width: 60, height: 60 }}
         />
       </Box>
       <Box bgColor="black" borderRadius="100%">
         <SocialIcon
-          url="https://www.instagram.com/gokai-labs"
+          url="https://www.instagram.com/greengardencs"
           bgColor="#4cfd27"
           style={{ width: 60, height: 60 }}
         />
