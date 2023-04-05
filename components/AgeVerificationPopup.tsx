fff00e2e33265ccaeb7132072b6234abaa1f072c
Jeremy <jeremydouchamps@gmail.com>
2023-04-05 12:35:06 +0700

    Update age verification

--- components/AgeVerificationPopup.tsx
+++ components/AgeVerificationPopup.tsx
@@ -7,28 +7,29 @@ type AgeVerificationPopupProps = {
   onVerify: () => void;
 };
 
-const AgeVerificationPopup = ({ onVerify }: AgeVerificationPopupProps) => {
-  const [isOpen, setIsOpen] = useState(true);
-
+function AgeVerificationPopup({ isOpen, onClose }) {
   useEffect(() => {
-    const isVerified = localStorage.getItem('ageVerified');
-    if (isVerified) {
-      setIsOpen(false);
-    }
-  }, []);
+    const handleClickOutside = (event) => {
+      if (isOpen && !event.target.closest("#age-verification-popup")) {
+        onClose();
+      }
+    };
+
+    document.addEventListener("click", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("click", handleClickOutside);
+    };
+  }, [isOpen, onClose]);
 
   const verifyAge = () => {
-    setIsOpen(false);
-    localStorage.setItem('ageVerified', 'true');
-    onVerify();
+    // Code for verifying age
+    onClose();
   };
 
-  if (!isOpen) {
-    return null;
-  }
-
   return (
-    <Box
+    isOpen && (
+      <Box
       pos="fixed"
       zIndex="100"
       top={0}
@@ -56,40 +57,42 @@ const AgeVerificationPopup = ({ onVerify }: AgeVerificationPopupProps) => {
           maxW="inherit"
           alt="Green Garden Dispensary - Cannabis Shop Phuket - 20 or older only"
           title="Green Garden Dispensary - Cannabis Shop Phuket - 20 or older only"
-          onClick={verifyAge}
-          onTouchStart={verifyAge}
-          onMouseDown={verifyAge}
-          onPointerDown={verifyAge}
         />
-        <Box as="h2" display="flex" flexDirection="row" alignItems="baseline" fontSize="2xl" mt={4} whiteSpace="nowrap">
+        <Box
+          as="h2"
+          display="flex"
+          flexDirection="row"
+          alignItems="baseline"
+          fontSize="2xl"
+          mt={4}
+          whiteSpace="nowrap"
+        >
           Are you 20 or older
           <ImgQuestion />
         </Box>
-        <button
+        <Box
+          as="span"
+          display="inline-flex"
+          color="ghostVerse.green.base"
+          borderColor="ghostVerse.green.base"
+          fontSize="xl"
+          borderWidth={1}
+          bgColor="ghostVerse.dark.lighter"
+          backdropFilter="blur(3px)"
+          py={2}
+          px={6}
+          mb={2}
+          mr="auto"
+          outline="none"
+          cursor="pointer"
           onClick={verifyAge}
           onTouchStart={verifyAge}
           onMouseDown={verifyAge}
           onPointerDown={verifyAge}
           onTouchEnd={verifyAge}
         >
-          <Box
-            as="span"
-            display="inline-flex"
-            color="ghostVerse.green.base"
-            borderColor="ghostVerse.green.base"
-            fontSize="xl"
-            borderWidth={1}
-            bgColor="ghostVerse.dark.lighter"
-            backdropFilter="blur(3px)"
-            py={2}
-            px={6}
-            mb={2}
-            mr="auto"
-            outline="none"
-          >
-            Yes, I am over 20
-          </Box>
-        </button>
+          Yes, I am over 20
+        </Box>
         <br />
         <Link href="https://ghostverse.org/">
           <Box
@@ -111,6 +114,7 @@ const AgeVerificationPopup = ({ onVerify }: AgeVerificationPopupProps) => {
         </Link>
       </Box>
     </Box>
+    )
   );
 };
 
