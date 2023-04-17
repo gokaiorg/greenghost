ac013fe5885d80f7e9e4ef8cdb7d5d5dda2a8b3e
Jeremy <jeremydouchamps@gmail.com>
2023-04-17 13:10:01 +0700

    Update sitemp

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -13,28 +13,7 @@ module.exports = {
       changefreq: config.changefreq,
       priority: config.priority,
       lastmod: config.autoLastmod ? new Date().toISOString() : undefined,
-      images: [],
-    }
+      images: [{ loc: 'https://green.gd/green-ghost-degen-weed-shop.png' }],
+    };
   },
-  additionalPaths: async (config) => [
-  await config.transform(config, '/'),
-  {
-    loc: 'https://green.gd',
-    changefreq: 'weekly',
-    priority: 0.5,
-    lastmod: new Date().toISOString(),
-    images: [
-      {
-        url: "https://green.gd/media/green-garden-dispensary-cannabis-shop-phuket-strain-orange-velvet-cover.webp",
-        caption: 'Green Garden Dispensary - Cannabis Shop Phuket - Orange Velvet',
-        alt: 'Green Garden Dispensary - Cannabis Shop Phuket - Orange Velvet',
-      },
-      {
-        url: `${process.env.SITE_URL}/media/green-garden-dispensary-cannabis-shop-phuket-strain-runtz-cover.webp`,
-        caption: 'Green Garden Dispensary - Cannabis Shop Phuket - Runtz',
-        alt: 'Green Garden Dispensary - Cannabis Shop Phuket - Runtz',
-      },
-    ],
-  },
-],
-}
+};
