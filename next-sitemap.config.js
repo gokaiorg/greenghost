aa6b3e329a35b38494ff24722018fcd2b511c639
Jeremy <jeremydouchamps@gmail.com>
2023-04-19 12:40:32 +0700

    Debug sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,25 +1,5 @@
-// next-sitemap.config.js
-const { products, getProductImageUrls } = require('./config/products.ts');
-
+/** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://green.gd',
-  changefreq: 'daily',
-  priority: 0.7,
-  sitemapSize: 5000,
+  siteUrl: process.env.SITE_URL || 'https://green.gd',
   generateRobotsTxt: true,
-  // Default transformation function
-  transform: async (config, path) => {
-    const product = products.find((p) => p.slug === path.slice(7)); // assuming path starts with '/product/'
-    if (product) {
-      const imageUrls = getProductImageUrls(product);
-      return {
-        loc: path, // => this will be exported as http(s)://<config.siteUrl>/<path>
-        changefreq: config.changefreq,
-        priority: config.priority,
-        lastmod: config.autoLastmod ? new Date().toISOString() : undefined,
-        images: imageUrls.map((url) => ({ loc: url })),
-      };
-    }
-    return null;
-  },
 };
