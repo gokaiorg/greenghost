db3f33eb80a7151f67494c73e57e7f29b9e7a1bf
Jeremy <jeremydouchamps@gmail.com>
2023-09-01 17:57:23 +0700

    Update menu

--- config/degens.ts
+++ config/degens.ts
@@ -17,7 +17,7 @@ export const degens: Degen[] = [
   {
     slug: 'hash',
     name: 'Hash',
-    price: 2000,
+    price: 1000,
     packaging: '1g',
     THC: '45% - 400mg/g',
     CBD: '1% - 10mg/g',
@@ -33,6 +33,26 @@ export const degens: Degen[] = [
       '/media/green-ghost-degen-weed-shop-menu-degen-hash-qr-code.webp',
     ],
     imgDesc: 'Green Ghost - Degen Weed Shop - Hash',
+  },
+  {
+    slug: 'pure-kief',
+    name: 'Pure Kief',
+    price: 600,
+    packaging: '150 microns - 1g',
+    THC: '60%',
+    CBD: 'undefined',
+    ingretiens: '100% Kief',
+    quantity: 6,
+    description:
+      "Our kief is a true masterpiece in the world of cannabis. Carefully crafted from a harmonious blend of premium strains, it offers an exquisite balance of flavors and effects that cater to the most discerning cannabis connoisseurs. With an impressive THC content of 60%, it packs a potent punch that delivers a euphoric and uplifting experience. What sets our kief apart is the meticulous 150-micron sieving process, ensuring that only the finest and most potent trichomes are collected. The result? A product that not only boasts exceptional potency but also enhances the flavor profiles of your favorite strains. Whether you're a seasoned enthusiast or new to kief, our product promises a delightful journey into the world of high-quality cannabis.",
+    descSeo:
+      'Our kief is a true masterpiece in the world of cannabis. Carefully crafted from a harmonious blend of premium strains, it offers an exquisite balance of flavors and effects that cater to the most discerning cannabis connoisseurs. With an impressive THC content of 60%, it packs a potent punch that delivers a euphoric and uplifting experience.',
+    images: [
+      '/media/green-ghost-degen-weed-shop-degen-pure-kief-product-01.webp',
+      '/media/green-ghost-degen-weed-shop-degen-pure-kief-cover.webp',
+      '/media/green-ghost-degen-weed-shop-degen-pure-kief-cover.webp',
+    ],
+    imgDesc: 'Green Ghost - Degen Weed Shop - Pure Kief',
   },
 ];
 
