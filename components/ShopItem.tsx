e04814096f03d02a5650a67da05beb1bcb65c81f
Jeremy <jeremydouchamps@gmail.com>
2023-09-06 20:31:01 +0700

    Add shops page

---
+++ components/ShopItem.tsx
@@ -0,0 +1,128 @@
+import { Box, Text } from '@chakra-ui/react';
+import Link from 'next/link';
+import { Shop } from '../config/shops';
+import { IconShop } from './IconShop';
+import { IconClock } from './IconClock';
+import { IconPin } from './IconPin';
+import { IconsReview } from './IconsReview';
+
+type ShopItemProps = {
+  shop: Shop;
+};
+
+export const ShopItem = ({ shop }: ShopItemProps) => {
+  return (
+    <Box display={'flex'} flexDirection={{ base: 'column', lg: 'row' }} mb={10}>
+      <Box
+        display={'flex'}
+        flexDirection={'column'}
+        width={{ base: '100%', lg: '65%' }}
+      >
+        <Box
+          display={'flex'}
+          alignItems={{ base: 'left', lg: 'center' }}
+          flexDirection={{ base: 'column', lg: 'row' }}
+        >
+          <Text
+            as={'h3'}
+            color={'white'}
+            fontSize={{ base: '2xl' }}
+            fontFamily={'CubicFive12'}
+            borderColor={'ghostVerse.color2.base'}
+            bgColor={'ghostVerse.dark.lighter'}
+            borderWidth={1}
+            backdropFilter={'blur(3px)'}
+            p={4}
+            mr={'2'}
+          >
+            {shop.name}
+          </Text>
+          <Link
+            href={`weed-shop-near-me/${shop.slug}`}
+            title={`${shop.name}`}
+            passHref
+          >
+            <Box
+              display={'flex'}
+              alignItems={'center'}
+              color={'ghostVerse.green.base'}
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                width={'40px'}
+              >
+                <path d="M12 15a3 3 0 100-6 3 3 0 000 6z" />
+                <path
+                  fillRule="evenodd"
+                  d="M1.323 11.447C2.811 6.976 7.028 3.75 12.001 3.75c4.97 0 9.185 3.223 10.675 7.69.12.362.12.752 0 1.113-1.487 4.471-5.705 7.697-10.677 7.697-4.97 0-9.186-3.223-10.675-7.69a1.762 1.762 0 010-1.113zM17.25 12a5.25 5.25 0 11-10.5 0 5.25 5.25 0 0110.5 0z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            </Box>
+          </Link>
+        </Box>
+        <Box
+          as={'div'}
+          color={'ghostVerse.blue.base'}
+          fontFamily={'vt323'}
+          fontSize={{ base: '2xl' }}
+          mb={4}
+        >
+          <Box display={'flex'} alignItems={'center'}>
+            <IconShop />
+            <Box ml={2} mb={1}>
+              {shop.details}
+            </Box>
+          </Box>
+          <Box display={'flex'} alignItems={'center'}>
+            <IconClock />
+            <Box ml={2} mb={1}>
+              {shop.hours}
+            </Box>
+          </Box>
+        </Box>
+        <Text
+          display={'flex'}
+          pr={{ base: '0', md: '10rem' }}
+          fontSize={{ base: '3xl', md: '3xl' }}
+          fontFamily={'vt323'}
+          _hover={{
+            color: 'ghostVerse.green.base',
+          }}
+        >
+          <Link
+            href={shop.addresLink}
+            title={`Green Ghost - ${shop.name} - Google Map`}
+            passHref
+          >
+            <IconPin />
+            {shop.address}
+          </Link>
+        </Text>
+        <Link
+          href={shop.reviewLink}
+          title={`Green Ghost - ${shop.name} - Add a Google Map Review`}
+          passHref
+        >
+          <IconsReview />
+        </Link>
+      </Box>
+      <Box
+        width={{ base: '100%', lg: 'auto' }}
+        ml={{ base: 0, lg: 'auto' }}
+        mt={8}
+      >
+        <iframe
+          src={shop.mapLink}
+          width="350"
+          height="250"
+          style={{ border: 0 }}
+          loading="lazy"
+          referrerPolicy="no-referrer-when-downgrade"
+        ></iframe>
+      </Box>
+    </Box>
+  );
+};
---
+++ pages/weed-shop-near-me/[slug].tsx
@@ -0,0 +1,129 @@
+import { GetStaticPaths, GetStaticProps } from 'next';
+import Head from 'next/head';
+
+import { useRouter } from 'next/router';
+import { getShops, Shop } from '../../config/shops';
+
+import { MainLayout } from '../../components/MainLayout';
+import { HeaderMenu } from '../../components/HeaderMenu';
+import { HeaderMenuButtons } from '../../components/HeaderMenuButtons';
+import { HomeSectionTitle } from '../../components/HomeSectionTitle';
+import { Box } from '@chakra-ui/react';
+
+import 'swiper/css';
+import 'swiper/css/effect-coverflow';
+
+import Image from 'next/image';
+import { FindUsAlso } from '../../components/FindUsAlso';
+
+interface ShopPageProps {
+  shop: Shop;
+}
+
+export default function ShopPage({ shop }: ShopPageProps) {
+  const router = useRouter();
+
+  if (router.isFallback) {
+    return <div>Loading...</div>;
+  }
+
+  const handleBack = () => {
+    window.history.back();
+  };
+
+  return (
+    <>
+      <Head>
+        <title>Green Ghost - {shop.name}</title>
+        <meta name="description" content={shop.descSeo} />
+        <meta property="og:title" content={`Green Ghost - ${shop.name}`} />
+        <meta property="og:description" content={shop.descSeo} />
+        <meta property="og:image" content={shop.images[1]} />
+        <meta property="og:image:width" content="1000" />
+        <meta property="og:image:height" content="1000" />
+        <meta
+          property="og:url"
+          content={`https://green.gd/weed-shop-near-me/${shop.slug}`}
+        />
+        <meta name="twitter:title" content={`Green Ghost - ${shop.name}`} />
+        <meta name="twitter:description" content={shop.descSeo} />
+        <meta name="twitter:image" content={shop.images[1]} />
+        <meta
+          name="twitter:url"
+          content={`https://green.gd/weed-shop-near-me/${shop.slug}`}
+        />
+      </Head>
+      <MainLayout>
+        <HeaderMenu>
+          <HeaderMenuButtons enabled={['auth']} />
+        </HeaderMenu>
+        <Box
+          cursor={'pointer'}
+          color={'ghostVerse.green.base'}
+          onClick={handleBack}
+        >
+          {'< back'}
+        </Box>
+        <Box display={'flex'} flexDirection={{ base: 'column', md: 'row' }}>
+          <HomeSectionTitle title={`${shop.name}`} />
+        </Box>
+        <Box display={'flex'} flexWrap={'wrap'}>
+          {shop.images.map((image, index) => (
+            <Box key={index} w={'33.33333%'}>
+              <Image src={image} width={500} height={500} alt={shop.name} />
+            </Box>
+          ))}
+        </Box>
+        <Box
+          borderColor={'ghostVerse.color2.base'}
+          bgColor={'ghostVerse.dark.lighter'}
+          borderWidth={1}
+          backdropFilter={'blur(3px)'}
+          width="full"
+          p={4}
+          my={4}
+          mb={10}
+          display={'inline-flex'}
+          flexDirection={'column'}
+          fontSize={'3xl'}
+          fontFamily={'vt323'}
+          whiteSpace="pre-line"
+        >
+          {shop.description}
+        </Box>
+        <FindUsAlso />
+      </MainLayout>
+    </>
+  );
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  //console.log('Generating paths...');
+  const shops = getShops();
+
+  const paths = shops.map((shop) => ({
+    params: { slug: shop.slug },
+  }));
+
+  return { paths, fallback: true };
+};
+
+export const getStaticProps: GetStaticProps<ShopPageProps> = async ({
+  params,
+}) => {
+  const shops = getShops();
+  const shop = shops.find((p) => p.slug === params?.slug);
+
+  if (!shop) {
+    return {
+      notFound: true,
+    };
+  }
+
+  return {
+    props: {
+      shop,
+    },
+    revalidate: 60 * 60, // 1 hour
+  };
+};
