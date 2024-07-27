3044ae91d688ce7864a1b3521e3269c6775c68a9
gokaiorg <jeremydouchamps@gmail.com>
2024-07-27 12:34:15 +0700

    Update menu

---
+++ components/media/IconHybrid.tsx
@@ -0,0 +1,12 @@
+import Image from 'next/image';
+
+export const IconHybrid = () => {
+  return (
+    <Image
+      src="/media/icon-hybrid.png"
+      width={30}
+      height={30}
+      alt="Green Ghost - Menu Weed Shop - Hybrid"
+    />
+  );
+};
