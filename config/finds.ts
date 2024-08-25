2fec1c1a4af8f7a1f589608703e75098e1cefe56
gokaiorg <jeremydouchamps@gmail.com>
2024-08-25 12:04:33 +0700

    Update links

--- config/finds.ts
+++ config/finds.ts
@@ -1,26 +1,8 @@
 export const find = [
   {
-    name: 'WEED.TH',
-    findUsLink: 'https://weed.th/shop/2ab3e52b-83f8-4a73-a96f-a60e04774a17',
-  },
-  {
     name: 'ThaiWeedGuide',
     findUsLink:
       'https://thaiweedguide.com/directory-dispensaries/listing/green-ghost/',
-  },
-  {
-    name: 'Trip Advisor',
-    findUsLink:
-      'https://www.tripadvisor.com.sg/Attraction_Review-g1215780-d26518105-Reviews-Green_Ghost-Karon_Phuket.html',
-  },
-  {
-    name: 'Restaurant Guru',
-    findUsLink: 'https://restaurantguru.com/Green-Ghost-Thailand',
-  },
-  {
-    name: 'Smoking Skunk',
-    findUsLink:
-      'https://smokingskunk.com/weed-in-thailand/green-ghost-degen-weed-shop-karon/',
   },
   {
     name: 'Phuket High Times',
--- config/shops.ts
+++ config/shops.ts
@@ -5,6 +5,10 @@ export type Shop = {
   address: string;
   addresLink: string;
   reviewLink: string;
+  tripadvisor: string;
+  weedin: string;
+  smokingskunk: string;
+  restauguru: string;
   description: string;
   descSeo: string;
   details: string;
@@ -21,6 +25,11 @@ export const shops: Shop[] = [
     addresLink: 'https://maps.app.goo.gl/9kVoX6VbjmroaHgJ6',
     reviewLink: 'https://g.page/r/CYOr82juHaKhEBM/review',
     details: 'Ghost loundge 👻💨',
+    tripadvisor:
+      'https://www.tripadvisor.com.sg/Attraction_Review-g297934-d28129979-Reviews-Green_Ghost-Rawai_Phuket.html',
+    weedin: 'https://weed.th/shop/2ab3e52b-83f8-4a73-a96f-a60e04774a17',
+    smokingskunk: '',
+    restauguru: 'https://restaurantguru.com/Green-Ghost-Thailand',
     description:
       "Welcome to the exclusive realm of Green Ghost Degen Weed Shop in Rawai, an avant-garde cannabis destination like no other. \n\n Green Ghost Degen Weed Shop in Rawai isn't just a dispensary; it's a lifestyle crafted for those who value both exclusivity and efficiency. Join us on a journey where quality, discretion, and convenience converge, setting a new standard for cannabis indulgence in Rawai.",
     descSeo:
@@ -41,6 +50,12 @@ export const shops: Shop[] = [
     addresLink: 'https://maps.app.goo.gl/GEuRczV3fnYnfy7z7',
     reviewLink: '#',
     details: 'Smoking area 💨',
+    tripadvisor:
+      'https://www.tripadvisor.com.sg/Attraction_Review-g1215780-d26518105-Reviews-Green_Ghost-Karon_Phuket.html',
+    weedin: 'https://weed.th/shop/fe835e3b-d043-4672-af43-8cc039aa013b',
+    smokingskunk:
+      'https://smokingskunk.com/weed-in-thailand/green-ghost-degen-weed-shop-karon/',
+    restauguru: '',
     description:
       "Your local cannabis haven nestled in the heart of beautiful Karon, just moments away from the vibrant Karon Market, the serene Karon Temple, and the pristine Karon Beach in Phuket, Thailand. \n\n Our Karon-based cannabis dispensary is your go-to destination for premium cannabis buds, a diverse selection of strains, delectable edibles, and more. We're dedicated to bringing the finest cannabis experience to the Karon community and its visitors. \n\n Convenience is key at Green Ghost Degen. Located near Tops Daily, you'll find it easy to access your favorite cannabis products. Whether you prefer the convenience of delivery, a quick and friendly pick-up service, or the speed of our drive-thru option, we've designed our services with your needs in mind. \n\n As a forward-thinking establishment, we embrace the digital age with NFTs (Non-Fungible Tokens) that offer unique and collectible cannabis-related digital assets. We also facilitate cryptocurrency transactions, making your shopping experience seamless and future-oriented. \n\n Green Ghost Degen Weed Shop is your gateway to exceptional cannabis products, all while immersing yourself in the charm and beauty of Karon, Phuket. Explore the wonders of this idyllic beachside town, and when you're ready to enhance your experience, we're here to serve you. \n\n Join us in Karon, where we merge cannabis culture with the beauty of southern Thailand.",
     descSeo:
@@ -61,6 +76,10 @@ export const shops: Shop[] = [
     addresLink: 'https://maps.app.goo.gl/QsL9wMfgn7WJj6bL9',
     reviewLink: 'https://g.page/r/CdEZf4I4iGn8EBE/review',
     details: 'Weed delivery only 🚚',
+    tripadvisor: '',
+    weedin: 'https://weed.th/shop/0d9dde2a-13bd-4167-b063-577af34971e8',
+    smokingskunk: '',
+    restauguru: '',
     description:
       "your ultimate destination for cannabis enthusiasts in the heart of Phuket Town. Conveniently situated on historic Thalang Road, just steps away from the bustling Sunday Market, our shop is your go-to spot for all things cannabis accessories. \n\n At Green Ghost Degen in Phuket Town, we've curated an impressive selection of high-quality cannabis gadgets that elevate your smoking experience. Explore our extensive range of grinders, bongs, rolling papers, and more – all designed to enhance your cannabis rituals. \n\n Located amidst the vibrant energy of Phuket's Sunday Market, our shop embraces the spirit of this cultural hub. After enjoying the market's eclectic offerings, drop by Green Ghost Degen Weed Shop to discover a world of cannabis accessories that cater to both beginners and seasoned connoisseurs. \n\n We're proud to provide an array of stylish, functional, and innovative products that cater to your unique needs. Our friendly and knowledgeable staff are here to assist you in finding the perfect accessory that complements your cannabis journey. \n\n In the heart of Phuket Town's historic Thalang Road, Green Ghost Degen Weed Shop invites you to explore the intersection of tradition and innovation. As we embrace the latest trends in cannabis culture, we invite you to immerse yourself in the rich tapestry of Phuket's vibrant past. \n\n Visit us today and discover the best in cannabis accessories, only at Green Ghost Degen Weed Shop in Phuket Town.",
     descSeo:
